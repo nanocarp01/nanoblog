4ae20d17c50181123bf4ba5df380ae1bfe591e2b
Fernando Heredia <71789047+nanocarp01@users.noreply.github.com>
2023-11-17 21:23:29 +0100

    Update settings.py
    cambio de puerto y debug para hacer deploy

--- blog/settings.py
+++ blog/settings.py
@@ -24,9 +24,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-^g2-+d_#^)6d0ob(w%byt$3dvfrufx=j&q(u#*aj@76534h^_x'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
+PORT = 8000
 
 
 # Application definition
