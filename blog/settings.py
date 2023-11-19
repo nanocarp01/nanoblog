aee8dd4f4a0fa4f7efdb843057c5b197aa8e12cf
Fernando Heredia <71789047+nanocarp01@users.noreply.github.com>
2023-11-19 10:19:58 +0100

    Update settings.py

--- blog/settings.py
+++ blog/settings.py
@@ -125,7 +125,7 @@ STATIC_URL = 'static/'
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+#STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
 # Default primary key field type
